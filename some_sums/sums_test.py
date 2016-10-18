fee350c61e525a31a6ffea025c4eaaa2575318a6
Keith Goodman <kwgoodman@gmail.com>
2016-10-18 13:13:11 -0700

    simplify unit tests

--- some_sums/sums_test.py
+++ some_sums/sums_test.py
@@ -1,10 +1,7 @@
 "Test sums functions."
 
-import warnings
-import traceback
 from itertools import permutations
 
-from nose.tools import ok_
 import numpy as np
 from numpy.testing import assert_array_almost_equal
 
@@ -19,7 +16,7 @@ def test_sums():
         yield unit_maker, func, arrays
 
 
-def arrays(dtypes, name):
+def array_iter(dtypes=DTYPES):
     "Iterator that yields arrays to use for unit testing."
 
     # nan and inf
@@ -29,10 +26,10 @@ def arrays(dtypes, name):
     yield np.ones((2, 0))
     yield np.ones((0, 2))
 
-    # Automate a bunch of arrays to test
+    # automate a bunch of arrays to test
     ss = {}
     ss[0] = {'size': 12, 'shapes': [(2, 6), (3, 4)]}
-    ss[1] = {'size': 16, 'shapes': [(2, 2, 4)]}
+    ss[1] = {'size': 24, 'shapes': [(2, 3, 4)]}
     ss[2] = {'size': 24, 'shapes': [(1, 2, 3, 4)]}
     for seed in (1, 2):
         rs = np.random.RandomState(seed)
@@ -42,10 +39,8 @@ def arrays(dtypes, name):
             for dtype in dtypes:
                 a = np.arange(size, dtype=dtype)
                 if issubclass(a.dtype.type, np.inexact):
-                    if name not in ('nanargmin', 'nanargmax'):
-                        # numpy can't handle eg np.nanargmin([np.nan, np.inf])
-                        idx = rs.rand(*a.shape) < 0.2
-                        a[idx] = inf
+                    idx = rs.rand(*a.shape) < 0.2
+                    a[idx] = inf
                     idx = rs.rand(*a.shape) < 0.2
                     a[idx] = nan
                     idx = rs.rand(*a.shape) < 0.2
@@ -54,70 +49,14 @@ def arrays(dtypes, name):
                 for shape in shapes:
                     yield a.reshape(shape)
 
-
-def unit_maker(func, array_func, decimal=5):
-    "Test that ss.sumXX gives the same output as np.sum."
-    fmt = '\nfunc %s | input %s (%s) | shape %s | axis %s | order %s\n'
-    fmt += '\nInput array:\n%s\n'
-    name = func.__name__
-    func0 = np.sum
-    for i, a in enumerate(array_iter(array_func, DTYPES, name)):
-        axes = range(-1, a.ndim)
-        for axis in axes:
-            actual = 'Crashed'
-            desired = 'Crashed'
-            actualraised = False
-            try:
-                # do not use a.copy() here because it will C order the array
-                actual = func(a, axis=axis)
-            except:
-                actualraised = True
-            desiredraised = False
-            try:
-                with warnings.catch_warnings():
-                    warnings.simplefilter("ignore")
-                    desired = func0(a, axis=axis)
-            except:
-                desiredraised = True
-            if actualraised and desiredraised:
-                pass
-            else:
-                tup = (name, 'a'+str(i), str(a.dtype), str(a.shape),
-                       str(axis), order_str(a), a)
-                err_msg = fmt % tup
-                if actualraised != desiredraised:
-                    if actualraised:
-                        fmt2 = '\nss.%s raised\nnp.sum ran\n\n%s'
-                    else:
-                        fmt2 = '\nss.%s ran\nnp.sum raised\n\n%s'
-                    msg = fmt2 % (name, traceback.format_exc())
-                    err_msg += msg
-                    ok_(False, err_msg)
-                assert_array_almost_equal(actual, desired, decimal, err_msg)
-
-
-# ---------------------------------------------------------------------------
-# Test with arrays that are not C ordered
-
-def test_strides():
-    "test sums functions with non-C ordered arrays"
-    for func in ss.get_functions():
-        yield unit_maker_strides, func
-
-
-def arrays_strides(dtypes=DTYPES):
-    "Iterator that yields non-C orders arrays."
-
-    # 2d
+    # non-contiguous arrays
     for dtype in dtypes:
         a = np.arange(12).reshape(4, 3).astype(dtype)
         yield a[::2]
         yield a[:, ::2]
         yield a[::2][:, ::2]
-
-    # 3d
     for dtype in dtypes:
-        a = np.arange(24).reshape(2, 3, 4).astype(dtype)
+        a = np.arange(60).reshape(3, 4, 5).astype(dtype)
         for start in range(2):
             for step in range(1, 2):
                 yield a[start::step]
@@ -127,24 +66,22 @@ def arrays_strides(dtypes=DTYPES):
                 yield a[start::step][::2][:, ::2]
 
 
-def unit_maker_strides(func, decimal=5):
+def unit_maker(func, arrays_func, decimal=5):
     "Test that ss.sumXX gives the same output as np.sum."
     fmt = '\nfunc %s | input %s (%s) | shape %s | axis %s | order %s\n'
     fmt += '\nInput array:\n%s\n'
-    fmt += '\nStrides: %s\n'
     name = func.__name__
     func0 = np.sum
-    for i, a in enumerate(array_iter(arrays_strides)):
+    for i, a in enumerate(arrays_func()):
         axes = range(-1, a.ndim)
         for axis in axes:
             # do not use a.copy() here because it will C order the array
             actual = func(a, axis=axis)
             desired = func0(a, axis=axis)
             tup = (name, 'a'+str(i), str(a.dtype), str(a.shape),
-                   str(axis), order_str(a), a, a.strides)
+                   str(axis), order_str(a), a)
             err_msg = fmt % tup
             assert_array_almost_equal(actual, desired, decimal, err_msg)
-            err_msg += '\n dtype mismatch %s %s'
 
 
 # ---------------------------------------------------------------------------
@@ -157,7 +94,7 @@ def test_unrolling():
         yield unit_maker, func, unroll_arrays
 
 
-def unroll_arrays(dtypes, name):
+def unroll_arrays(dtypes=DTYPES):
     "Iterator that yields arrays to use for unit testing."
     for ndim in (2,):
         rs = np.random.RandomState(ndim)
@@ -174,8 +111,8 @@ def unroll_arrays(dtypes, name):
 # ---------------------------------------------------------------------------
 # utility functions
 
-def array_iter(arrays_func, *args):
-    for a in arrays_func(*args):
+def arrays():
+    for a in array_iter():
         if a.ndim < 2:
             yield a
         elif a.ndim == 3:
@@ -194,5 +131,5 @@ def order_str(a):
     if f.f_contiguous:
         string.append("F")
     if len(string) == 0:
-        string.append("Neither")
-    return ", ".join(string)
+        string.append("N")
+    return ",".join(string)
