b699b874d538fad6c45563f574ff63affe520257
Keith Goodman <kwgoodman@gmail.com>
2016-10-24 12:44:29 -0700

    try running benchmark on travis

--- tools/test-installed-some_sums.py
+++ tools/test-installed-some_sums.py
@@ -40,6 +40,8 @@ parser.add_option("-m", "--mode",
                        "passed to nosetests -A [default: %default]")
 (options, args) = parser.parse_args()
 
+some_sums.bench()
+
 result = some_sums.test(options.mode,
                         verbose=options.verbose,
                         extra_argv=args,
