ed829d881728279a32e0cedd1c2cbf163ae681c8
Keith Goodman <kwgoodman@gmail.com>
2016-11-02 13:28:13 -0700

    attempt to get avx working on windows

--- some_sums/src/sums.h
+++ some_sums/src/sums.h
@@ -179,6 +179,7 @@ reducer02(PyObject *args,
 
 #if defined(_MSC_VER)
      #include <intrin.h>
+     #include <immintrin.h>
 #elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
      #include <x86intrin.h>
 #endif
